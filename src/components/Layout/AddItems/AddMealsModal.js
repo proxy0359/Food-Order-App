605661d47bfeab000ea70dd9f8d083e53a96236b
proxyjem jackprox <jemjem.santos03@gmail.com>
2022-12-12 23:46:45 +0800

    updated the API for getting the data in firebase

--- src/components/Layout/AddItems/AddMealsModal.js
+++ src/components/Layout/AddItems/AddMealsModal.js
@@ -1,14 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../../UI/Modal";
 import style from "./AddMealsModal.module.css";
 
 const AddMealsModal = (props) => {
+  const [nameVal, setNameVal] = useState("");
+  const [descriptionVal, setDescriptionVal] = useState("");
+  const [priceVal, setPriceVal] = useState();
+
+  const nameIsValid = nameVal.trim() !== "";
+  const descriptionIsValid = descriptionVal.trim() !== "";
+  const priceIsVal = priceVal > 1;
+
+  const nameOnChangeHandler = (event) => {
+    setNameVal(event.target.value);
+  };
+
+  const desChangeHandler = (event) => {
+    setDescriptionVal(event.target.value);
+  };
+
+  const priceChangeHandler = (event) => {
+    setPriceVal(event.target.value);
+  };
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    if (!nameIsValid) {
+      return;
+    }
+    if (!descriptionIsValid) {
+      return;
+    }
+    if (!priceIsVal) {
+      return;
+    }
+
+    await fetch(
+      "https://food-order-app-fed0b-default-rtdb.asia-southeast1.firebasedatabase.app/products.json",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: nameVal,
+          description: descriptionVal,
+          price: priceVal,
+        }),
+      }
+    );
+
+    setDescriptionVal("");
+    setPriceVal("");
+
+    setNameVal("");
+  };
   return (
     <Modal onClick={props.onClick}>
-      <div className={style.actions}>
-        <button className={style.button}>Send</button>
-        <button className={style["button--alt"]}>Close</button>
-      </div>
+      <form onSubmit={formSubmitHandler} className={style.input}>
+        <label htmlFor="productName">Product Name</label>
+        <input
+          type="text"
+          id="productName"
+          onChange={nameOnChangeHandler}
+          value={nameVal}
+        />
+        <label htmlFor="productDescription">Description</label>
+        <input
+          type="text"
+          id="productDescription"
+          onChange={desChangeHandler}
+          value={descriptionVal}
+        />
+        <label htmlFor="productPrice">Price</label>
+        <input
+          type="text"
+          id="productPrice"
+          onChange={priceChangeHandler}
+          value={priceVal}
+        />
+
+        <div className={style.actions}>
+          <button className={style.button}>Send</button>
+          <button className={style["button--alt"]} onClick={props.onClick}>
+            Close
+          </button>
+        </div>
+      </form>
     </Modal>
   );
 };
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = (props) => {
-  const mealList = DUMMY_MEALS.map((meals) => (
+  const [meals, setMeal] = useState([]);
+
+  useEffect(() => {
+    const productItems = async () => {
+      const data = await fetch(
+        "https://food-order-app-fed0b-default-rtdb.asia-southeast1.firebasedatabase.app/products.json"
+      );
+
+      const convertData = await data.json();
+
+      let loadedProducts = [];
+
+      for (let i in convertData) {
+        const price = convertData[i].price;
+        const updatedPr = parseFloat(price);
+        loadedProducts.unshift({
+          name: convertData[i].name,
+          id: i,
+          description: convertData[i].description,
+          price: updatedPr,
+        });
+      }
+
+      setMeal(loadedProducts);
+    };
+
+    productItems();
+  }, [setMeal, meals]);
+
+  const mealList = meals.map((meals) => (
     <Card>
       <MealItem
         inputVal={props.inputVal}
