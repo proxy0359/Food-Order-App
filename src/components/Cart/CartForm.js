78d213c6d4cac2531e500a438f6732b27363cb6b
proxyjem jackprox <jemjem.santos03@gmail.com>
2022-12-14 20:55:02 +0800

    error checker patched

--- src/components/Cart/CartForm.js
+++ src/components/Cart/CartForm.js
@@ -1,11 +1,23 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import style from "./cartForm.module.css";
 
+const nameIsValid = (name) => name !== "";
+const streetIsValid = (street) => street !== "";
+const cityIsValid = (city) => city !== "";
+const postalIsValid = (postal) => postal === 5;
+
 const CartForm = (props) => {
+  const [validInput, setValidInput] = useState({
+    name: true,
+    street: true,
+    postal: true,
+    city: true,
+  });
   const nameRef = useRef();
   const streetRef = useRef();
   const postalCode = useRef();
   const cityRef = useRef();
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
 
@@ -13,12 +25,31 @@ const CartForm = (props) => {
     const enteredStreet = streetRef.current.value;
     const enteredPostal = postalCode.current.value;
     const enteredCity = cityRef.current.value;
+
+    const validName = nameIsValid(enteredName);
+    const validStreet = streetIsValid(enteredStreet);
+    const validPostal = postalIsValid(enteredPostal);
+    const validCity = cityIsValid(enteredCity);
+
+    const formIsValid = validName && validStreet && validPostal && validCity;
+
+    setError({
+      name: validName,
+      street: validStreet,
+      postal: validPostal,
+      city: validCity,
+    });
+
+    if (formIsValid) {
+      return;
+    }
   };
   return (
     <form onSubmit={onSubmitHandler} className={style.form}>
       <div className={style.control}>
         <label htmlFor="name">name</label>
         <input type="text" name="name" id="name" ref={nameRef} />
+        {!error.name && <p>Please a valid Name!</p>}
       </div>
       <div className={style.control}>
         <label htmlFor="street">Street</label>
